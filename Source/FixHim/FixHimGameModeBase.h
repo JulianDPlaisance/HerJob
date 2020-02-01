e2036be1c80fb2c92c0b50aef299f9e9169dc8a1
Julian Dane Plaisance <jplais8@lsu.edu>
2020-01-31 20:05:50 -0600

    default unreal

---
+++ Source/FixHim/FixHimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FixHimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIXHIM_API AFixHimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
